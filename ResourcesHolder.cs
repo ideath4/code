4f826c6db946d5e11594c012057f79790fd5b9f0
ideath4 <ideath4@gmail.com>
2020-06-23 11:44:49 +0300

    initial commit

---
+++ ResourcesHolder.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "IT/ResourceHolder")]
+public class ResourcesHolder : ScriptableObject
+{
+    static ResourcesHolder prefab;
+    public static ResourcesHolder Prefab
+    {
+        get
+        {
+            if(prefab == null)
+            {
+                prefab = Resources.Load<ResourcesHolder>("IT/ResourcesHolder");
+                if(prefab == null)
+                {
+                    Debug.LogError("Can't load ResourceHolder at Resources/IT/ResourcesHolder");
+                }
+            }
+            return prefab;
+        }
+    }
+
+    public Color HeaderButtonDisabledColor;
+
+    public List<SpriteResourceData> sprites = new List<SpriteResourceData>();
+
+    public Sprite GetSprite(string key)
+    {
+        var data = sprites.Find(s => s.Key.ToLower() == key.ToLower());
+        if (data == null)
+            return null;
+        return data.sprite;
+    }
+    
+    public Color GetTextOutlineColor(string Key)
+    {
+        var data = sprites.Find(s => s.Key.ToLower() == Key.ToLower());
+        if (data == null)
+            return Color.white;
+        return data.textOutlineColor;
+    }
+
+}
+
+[System.Serializable]
+public class ResourceData
+{
+    public string Key;
+}
+[System.Serializable]
+public class SpriteResourceData : ResourceData
+{
+    public Sprite sprite;
+    public Color textOutlineColor;
+}
---
+++ SwipeElement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwipeElement : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public SwipeBody parent;
+    float maxLength
+    {
+        get
+        {
+            return Screen.width * currentOptions.MaxLengthPerc;
+        }
+    }
+    public bool interact = false;
+    Vector2 startSwipe;
+    public SwipeAnimPoint targetPoint;
+    SwipeOptions currentOptions = new SwipeOptions();
+
+    public void SetOptions(SwipeOptions opt)
+    {
+        currentOptions = opt;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!interact)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPoint.position, currentOptions.backToPosSpeed * Time.deltaTime);
+            transform.localScale = Vector3.Lerp(transform.localScale, targetPoint.scale, currentOptions.backToPosSpeed * Time.deltaTime);
+            return;
+        }
+
+        if (parent.current != this)
+            return;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            MouseUP();
+            return;
+        }
+        float dif = Input.mousePosition.x - startSwipe.x;
+        float perc = Mathf.Clamp01(Mathf.Abs(dif / maxLength));
+        if (perc < currentOptions.minPercentForAnim)
+            return;
+        if(perc >= 1)
+        {
+            MouseUP();
+            return;
+        }
+
+        if (dif > 0)
+        {
+            parent.SetPercAnim(parent.centerPoint, parent.rightPoint, perc * currentOptions.animationPercent, -1);
+        }
+        else if (dif < 0)
+        {
+            parent.SetPercAnim(parent.centerPoint, parent.leftPoint, perc * currentOptions.animationPercent, 1);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        interact = true;
+        startSwipe = Input.mousePosition;
+    }
+
+    void MouseUP()
+    {
+        interact = false;
+        if (parent.current != this)
+            return;
+        float dif = Input.mousePosition.x - startSwipe.x;
+        float perc = Mathf.Clamp01(Mathf.Abs(dif / maxLength));
+
+        if (dif > 0)
+        {
+            if (perc == 1)
+            {
+                parent.SwipeRight();
+            }
+            else
+            {
+                parent.Refresh();
+            }
+        }
+        else if (dif < 0)
+        {
+            if (perc == 1)
+            {
+                parent.SwipeLeft();
+            }
+            else
+            {
+                parent.Refresh();
+            }
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        MouseUP();
+    }
+
+
+}
